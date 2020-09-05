8be17d530ec22d53ba0e4d9c95cc70d0e33b2dc6
hectorromerodev <hectorromero.developer@gmail.com>
2020-09-04 17:10:40 -0700

    ADD Init logic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor() {}
+  @ViewChild('imageCanvas', { static: false }) canvas: any;
+  drawing = false;
+  selectedColor = '#263238';
+  colors = ['#263238', '#9e9e9e', '#795548', '#ff5722', '#ff9800', '#ffc107', '#ffeb3b', '#4caf50', '#2196f3', '#f44336'];
+  lineWidth = 5;
 
+  constructor() { }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  startDrawing(ev) {
+    console.log('start ', ev);
+    this.drawing = true;
+  }
+
+  endDrawing(ev) {
+    console.log('end ', ev);
+    this.drawing = false;
+  }
+
+  moved(ev) {
+    console.log('moved ', ev);
+
+  }
 }
